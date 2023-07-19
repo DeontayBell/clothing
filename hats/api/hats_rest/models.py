9eda555074035f63da6af06a9eca544e1529d0c7
Deontay <dbell9823@gmail.com>
2023-07-19 14:40:56 -0500

    poller done. VO is done, models updated, working on views

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200)
+    closet_name = models.CharField(max_length=100)
+
+
+class Hat(models.Model):
+    name = models.CharField(max_length=100)
+    fabric = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    url = models.URLField(max_length=500)
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hat",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Hat
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+@require_http_methods(["GET", "POST"])
+def api_hat(request):
+    if request.method == "GET":
+        hat = Hat.objects.all()
+        return JsonResponse(
+            {"Hats": hat},
+
+        )
